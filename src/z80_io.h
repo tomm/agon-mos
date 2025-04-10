9ba639334fe2aac1eb1a3556bcbbff8ed86f2a51
Tom Morton <tomm@riseup.net>
2025-04-10 21:01:33 +0100

    Faster and smaller io_in/io_out implementations (using address_space attribute)

--- src/z80_io.h
+++ src/z80_io.h
@@ -3,38 +3,27 @@
 
 #include <stdint.h>
 
-/* io_in and io_out look weird, but there are reasons... */
-static inline uint8_t io_in(uint8_t addr)
+/**
+ * The following magical incantations: __attribute__((address_space(3))
+ * cause the emission of IO in/out instructions.
+ */
+
+static inline uint8_t io_in(int addr)
 {
-	uint8_t out;
-	asm volatile(
-	    "ld b,0\n"
-	    "in b,(c)\n"
-	    :"=bc"(out)
-	    :"c"(addr)
-	);
-	return out;
+	return *((volatile uint8_t __attribute__((address_space(3)))*)addr);
 }
 
-static inline void io_out(uint8_t addr, uint8_t value)
+static inline void io_out(int addr, uint8_t value)
 {
-	asm volatile(
-	    "ld a,b\n"
-	    "ld b,0\n"
-	    "out (c),e\n"
-	    "ld b,a\n"
-	    :
-	    :"e"(value),"c"(addr)
-	    :"a"
-	);
+	*((volatile uint8_t __attribute__((address_space(3)))*)addr) = value;
 }
 
-static inline void io_setreg(uint8_t addr, uint8_t bits)
+static inline void io_setreg(int addr, uint8_t bits)
 {
 	io_out(addr, io_in(addr) | bits);
 }
 
-static inline void io_resetreg(uint8_t addr, uint8_t bits)
+static inline void io_resetreg(int addr, uint8_t bits)
 {
 	io_out(addr, io_in(addr) & (0xff ^ bits));
 }
